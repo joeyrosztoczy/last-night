17f2de7936448f5e897d0c0379bf9bef469b63c4
Matthew <mvernhein@gmail.com>
2014-07-11 14:12:54 -0500

    done with party CRUD

--- app/controllers/home.rb
+++ app/controllers/home.rb
@@ -1,3 +1,15 @@
 get '/home' do 
 	erb :home
 end
+
+
+post '/home/create' do 
+	Party.create(name: params[:name], location: params[:location])
+	redirect "/home"
+end
+
+post '/home/delete/:party_id' do 
+	@party = Party.find(params[:party_id])
+	@party.destroy
+	redirect 'home'
+end
