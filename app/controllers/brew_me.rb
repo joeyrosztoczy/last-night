296c7e3c51df789d0ecd8cc3d8bd16d3c8e017e0
Spencer Olson <spence1818@gmail.com>
2014-07-11 14:45:30 -0500

    finished brew me page

--- app/controllers/brew_me.rb
+++ app/controllers/brew_me.rb
@@ -1,5 +1,12 @@
 get '/party/:party_id' do 
-  session[:user_id] = 1
   @party = Party.find(params[:party_id])
-  erb :_list_partiers
+  erb :party
+end
+
+post '/brewme' do 
+  @party = Party.find(params[:party_id])
+  puts "in the brewme route! party id is: #{@party.id}"
+  @last_night = LastNight.where(party_id: @party.id, user_id: session[:user_id]).first
+  @last_night.update(brew_count: @last_night.brew_count + 1)
+  erb :_list_partiers, layout: false
 end
--- public/js/application.js
+++ public/js/application.js
@@ -7,4 +7,20 @@ $(document).ready(function () {
     request.done(function () { window.location = "/"; });
   });
 
+  $("#brew_me").on("submit", function(event){
+    event.preventDefault();
+    console.log("button was clicked");
+
+    $.post('/brewme', $(this).serialize(), function(response){
+    	console.log("Response is " + response);
+    	$("#partier_list").empty();
+    	$("#partier_list").append(response);
+
+
+    });
+    
+  });
+
+
+ 
 });
